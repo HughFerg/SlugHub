34a05fb9bdba5d4d548d8b57258c44fb8d477edf
hughferg <hughferguson25@gmail.com>
2019-06-27 19:25:00 -0400

    text fix

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,7 +9,7 @@ const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     {/* <Link to="/page-2/">Go to page 2</Link> */}
-    <div style={{ fontSize: `600%`, height: `200%` }} >welcome to slughub, my dudes</div>
+    <div class="intro-text">welcome to slughub, my dudes</div>
   </Layout>
 )
 
